8e8ac7935e2753d8b6120e6a51de4efaa8570f62
Iman <mohammedelkhamal@Mohammeds-MacBook-Pro.local>
2026-05-14 10:53:47 +0100

    feat: creation des fichier index.html et script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Écouter la soumission du formulaire 
+/* À COMPLÉTER */ 
+// Empêcher le rechargement de la page 
+// /* À COMPLÉTER */ 
+// Construire l'objet examen 
+const examen = { /* À COMPLÉTER */ 
+         /* À COMPLÉTER */
+          /* À COMPLÉTER */ 
+          /* À COMPLÉTER */ 
+         questions: []
+ };
+ // Construire la clé de stockage basée sur le propriétaire 
+ // /* À COMPLÉTER */ 
+
+ // Lire les examens existants (ou tableau vide si rien) 
+ /* À COMPLÉTER */ 
+
+ // Ajouter le nouvel examen et sauvegarder 
+ /* À COMPLÉTER */
+  /* À COMPLÉTER */ 
+ alert('Examen ajouté avec succès !'); 
+ this.reset();
